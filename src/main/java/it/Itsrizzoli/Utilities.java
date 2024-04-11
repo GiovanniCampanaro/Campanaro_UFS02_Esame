2b486921a762de1538c113aeb49a81a81c59bd2b
GiovanniCampanaro <giovanni.campanaro@itsrizzoli.it>
2024-04-11 16:08:03 +0200

    versione copleta 3 con Utilities fatto bene

--- src/main/java/it/Itsrizzoli/Utilities.java
+++ src/main/java/it/Itsrizzoli/Utilities.java
@@ -2,35 +2,48 @@ package it.Itsrizzoli;
 
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.util.List;
 
 public class Utilities {
     public static String readTextFrom(String fname) {
-        InputStream is;
-        is = Utilities.class.getClassLoader().getResourceAsStream(fname);
+        InputStream is = Utilities.class.getClassLoader().getResourceAsStream(fname);
         if (is == null) {
             throw new IllegalArgumentException("Il file " + fname + " non esiste");
         }
 
+        BufferedReader buf = new BufferedReader(new InputStreamReader(is));
+
         StringBuilder fileAsString = new StringBuilder();
-        try (Reader reader = new InputStreamReader(is)) {
-            int character;
-            while ((character = reader.read()) != -1) {
-                fileAsString.append((char) character);
+        String line;
+
+        try {
+            while ((line = buf.readLine()) != null) {
+                fileAsString.append(line).append("\n");
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                buf.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         return fileAsString.toString();
     }
 
     public static <T> T loadFromJson(String filename, Class<T> classOfT) {
-        String json = readTextFrom(filename);
-        Gson gson = new Gson();
-        return gson.fromJson(json, classOfT);
+        try (Reader reader = new InputStreamReader(Utilities.class.getClassLoader().getResourceAsStream(filename))) {
+            Gson gson = new Gson();
+            return gson.fromJson(reader, classOfT);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
